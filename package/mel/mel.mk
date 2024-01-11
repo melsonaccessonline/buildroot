2e73b97e6bd3ec8a94f5b73da2dd451bf4b50413
melsonaccessonline <155618053+melsonaccessonline@users.noreply.github.com>
2024-01-11 22:20:57 +0530

    Update mel.mk

--- package/mel/mel.mk
+++ package/mel/mel.mk
@@ -5,18 +5,15 @@
 ################################################################################
 
 MEL_VERSION = 1.0
-MEL_SITE = package/mel/src
+MEL_SITE = ./package/mel/src
 MEL_SITE_METHOD=local#
 MEL_INSTALL_TARGET:=YES
 define MEL_BUILD_CMDS
-    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
+    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
 endef
 
 define MEL_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/mel  $(TARGET_DIR)/bin
-endef
-define MEL_PERMISSIONS
-       /bin/mel f 4755 0 0 - - - - - 
+    $(INSTALL) -D -m 0755 $(@D)/mel  $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
