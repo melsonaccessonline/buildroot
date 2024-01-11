7dc855ba4d9c3d4e2e78683bda615d0a4e097f3b
melsonaccessonline <155618053+melsonaccessonline@users.noreply.github.com>
2024-01-11 22:12:09 +0530

    Update mel.mk

--- package/mel/mel.mk
+++ package/mel/mel.mk
@@ -7,12 +7,16 @@
 MEL_VERSION = 1.0
 MEL_SITE = package/mel/src
 MEL_SITE_METHOD=local#
+MEL_INSTALL_TARGET:=YES
 define MEL_BUILD_CMDS
-    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
 endef
 
 define MEL_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/mel  $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D -m 0755 $(@D)/mel  $(TARGET_DIR)/bin
+endef
+define MEL_PERMISSIONS
+       /bin/mel f 4755 0 0 - - - - - 
 endef
 
-$(eval $(autotools-package))
+$(eval $(generic-package))
