5567d3592b698ba6c601f5b3fa88619ece00e5c1
melsonaccessonline <155618053+melsonaccessonline@users.noreply.github.com>
2024-01-11 21:35:32 +0530

    Update mel.mk

--- package/mel/mel.mk
+++ package/mel/mel.mk
@@ -6,7 +6,7 @@
 
 MEL_VERSION = 1.0
 MEL_SITE = package/mel/src
-MEL_SITE_METHOD=local
+MEL_SITE_METHOD=local#
 define MEL_BUILD_CMDS
     $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
 endef
